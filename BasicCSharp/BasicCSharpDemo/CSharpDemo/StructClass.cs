609fc9927548b657c69451a48866457aa975b159
WangGang <gwang@thoughtworks.com>
2018-07-27 21:12:41 +0800

    add struct

---
+++ BasicCSharp/BasicCSharpDemo/CSharpDemo/StructClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BasicCSharpDemo.CSharpDemo
+{
+    internal struct Point
+    {
+        public int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class StructClass
+    {
+        public static void devTest()
+       {
+            Console.WriteLine("dev test");
+        }
+    }
+}
