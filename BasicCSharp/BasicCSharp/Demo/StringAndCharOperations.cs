016aa1573f43716f76a9234287c3f3a57cc7a0e1
WangGang <gwang@thoughtworks.com>
2018-08-01 15:31:26 +0800

    test diff

--- BasicCSharp/BasicCSharp/Demo/StringAndCharOperations.cs
+++ BasicCSharp/BasicCSharp/Demo/StringAndCharOperations.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using BasicCSharpDemo.CSharpDemo;
 using Xunit;
 
 namespace BasicCSharp.Demo
 {
+    /*
+     * 测试internal修饰符
+     * 当internal修饰符只能在一个工程中使用，不能跨工程
+     */
      public class StringAndCharOperations
     {
          public void test()
         {
             Console.WriteLine("hello world!");
             //Assert.Equal(true,true);
+            VisitModifier vm =new VisitModifier();
+            int a =VisitModifier.b;
+            
+            Console.WriteLine("test git checkout");
         }
 
     }
