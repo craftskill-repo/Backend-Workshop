7f892d256e784f4f65456d1094deccf21f072399
WangGang <gwang@thoughtworks.com>
2018-08-02 21:04:22 +0800

    add dev1 confliect

--- BasicCSharp/BasicCSharp/Demo/StringAndCharOperations.cs
+++ BasicCSharp/BasicCSharp/Demo/StringAndCharOperations.cs
@@ -18,6 +18,8 @@ namespace BasicCSharp.Demo
             int a =VisitModifier.b;
             
             Console.WriteLine("test git checkout");
+            
+            Console.WriteLine("dev1 confliect!");
         }
 
     }
